99914906f3b14f405c5efdc684928009e24d78bf
GunwooJo <kanggi1997@naver.com>
2025-01-01 20:48:43 +0900

    refactor: PlantImage를 독립적인 컬렉션으로 관리하지 않도록 수정

--- src/main/kotlin/study/my_plant/models/PlantImage.kt
+++ src/main/kotlin/study/my_plant/models/PlantImage.kt
@@ -1,9 +1,7 @@
 package study.my_plant.models
 
-import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDate
 
-@Document
 class PlantImage (
     val date: LocalDate = LocalDate.now(),
     val imageUrl: String
